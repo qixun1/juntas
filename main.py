a6c289a1c7b4cb58543e3fbf363b49d7d66f4877
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2021-11-14 23:39:29 +0800

    Update main.py
    Modified K-Junta tester to also learn while testing

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-import random
 import numpy as np
-
-
-def binary_search(I, f, x, z):
-    if len(I) == 1:
-        return I
-    t = len(I) // 2
-    indices = np.concatenate(t)
-    x_prime = x.copy()
-    x_prime[indices] = z[indices]
-
-    if f(x) != f(x_prime):
-        binary_search(I[:t], f, x, x_prime)
-    binary_search(I[t:], f, x_prime, z)
-
+import random
 
 def partition_coordinates(s, n):
     I = {}
@@ -24,33 +10,38 @@ def partition_coordinates(s, n):
         I[b].append(i)
     return I
 
+def binary_search(I, f, x, z):
+    if len(I) == 1:
+        return I
+    t = len(I) // 2
+    indices = np.intersect1d(np.where(x != z), np.concatenate(I[:t]))
+    x_prime = x.copy()
+    x_prime[indices] = z[indices]
 
-def junta_test(f, k, eps, n):
-    # randomly partition coordinates into s sets
-    s = (10 ** 20) * (k ** 9) / (eps ** 5)
-    I = partition_coordinates(s, n)
-    r = 12 * (k + 1) / eps
+    if f(x) != f(x_prime):
+        return binary_search(I[:t], f, x, x_prime)
+    return binary_search(I[t:], f, x_prime, z)
+
+def Junta(f, k, eps, n, s, r):
+    relavants = []
     l = 0
-
+    I = list(partition_coordinates(s, n).values())
+    print("parts ",len(I))
     S = I.copy()
-    # to be replaced with real data
-    for _ in range(r):
-        x, y = _
-        # generate x,y
+    for _ in range(int(r)):
+        x, y = sample(n),sample(n)
         z = x.copy()
         indices = np.concatenate(S)
         z[indices] = y[indices]
-
         if f(x) != f(z):
-            part = binary_search(S, f, x, z)
-            # remove
+            part = binary_search(S, f, x, z)[0]
+            relavants.append(part)
+            if len(relavants) % 10 == 0:
+                print(len(relavants)," in ",_)
             S.remove(part)
             l += 1
             if l > k:
-                return False
-    return True
-
-
-if __name__ == '__main__':
-    # preprocess data
-    junta_test()
+                print("NOT ",k,"-JUNTA")
+                return relavants
+    print(k,"-JUNTA")
+    return relavants
